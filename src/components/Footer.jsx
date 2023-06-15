a0a9bd600f043703ca7eac783d10959b5626609a
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-06-15 12:08:25 -0300

    Minor footer adjustment - Link to GitHub profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Footer() {
   return (
     <footer>
-      <p>©2023 Eduardo Brandão</p>
+      <Link to="https://github.com/oeduardobrandao">
+        <p>©2023 Eduardo Brandão</p>
+      </Link>
     </footer>
   )
 }
